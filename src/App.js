10eea782f071321ea6a024c886786e1045180edc
heganjr <jacobhegan@hotmail.com>
2022-02-13 22:01:07 +0800

    added header component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import Experience from "./components/Experience";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+const Header = () => {
+  return (
+    <header id="header-banner">
+      <h1>Jacob Hegan</h1>
+      <nav>
+        <ul>
+          <li id="nav-link">
+            <a href="#nav-scroll">About Me</a>
+          </li>
+          <li id="nav-link">
+            <a href="#nav-scroll-2">Work</a>
+          </li>
+          <li id="nav-link">
+            <a href="#contact-me-heading">Contact Me</a>
+          </li>
+          <li id="nav-link">
+            <a
+              href="https://drive.google.com/file/d/124F7GKSeacH0dmhmA2D_8GeBxlgMb0nV/view?usp=sharing"
+              target="blank"
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
