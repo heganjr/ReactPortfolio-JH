0371f53963b4112ebc4561d142130d4548c99436
heganjr <jacobhegan@hotmail.com>
2022-02-14 00:02:37 +0800

    experience component added images added

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,1 +1,25 @@
+import jacobImage from "../images/ruby-dog.jpg";
 
+const Experience = () => {
+  return (
+    <div class="background-image">
+      <ul id="text-experience">
+        <p>Experience includes:</p>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Express.js</li>
+        <li>MySQL</li>
+        <li>NoSQL</li>
+        <li>Node.js</li>
+      </ul>
+      <img
+        class="personal-image"
+        src={jacobImage}
+        alt="Jacob Hegan"
+      />
+    </div>
+  );
+};
+
+export default Experience;
